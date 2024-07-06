063d695f8934de60c460d973489a566c6cd3210c
ruiallen <rw88251@uga.edu>
2024-07-06 12:45:02 -0400

    Added win and lose check

--- game.py
+++ game.py
@@ -62,6 +62,8 @@ class Board():
                 self.board[x1][y1] = 2
                 cnt+=1
             else:continue
+        self.score = 0
+        
     def get_value(self,x,y):
         return self.board[x][y]
     
@@ -75,14 +77,12 @@ class Board():
                 if self.board[x][y] == 0:
                     empty_list.append((x,y))
         return empty_list
-    
+
     def spawn_new_block(self):
         empty_list = self.get_empty_cell()
         coord = random.choice(empty_list)
         x,y = coord[0],coord[1]
         self.board[x][y] = 2
-    
-
     #define move operations
     #first move all the blocks to the right place, then merge, then move again because possible empty cell appeasr
     def move_up(self):
@@ -163,6 +163,7 @@ class Board():
                 if self.get_value(x,y) == self.get_value(to_merge,y) and self.get_value(x,y)!=0: # need to merge
                     val = self.get_value(x,y)
                     self.set_value(x,y,val+val)
+                    self.score = max(self.score,val+val)
                     self.set_value(to_merge,y,0)
                     x+=2
                 else:
@@ -177,11 +178,11 @@ class Board():
                 if self.get_value(x,y) == self.get_value(to_merge,y) and self.get_value(x,y)!=0: # need to merge
                     val = self.get_value(x,y)
                     self.set_value(x,y,val+val)
+                    self.score = max(self.score,val+val)
                     self.set_value(to_merge,y,0)
                     x-=2
                 else:
                     x-=1
-
     def merge_left(self):
         #from left to right
         for x in range(4):
@@ -191,11 +192,11 @@ class Board():
                 if self.get_value(x,y) == self.get_value(x,to_merge) and self.get_value(x,y)!=0: # need to merge
                     val = self.get_value(x,y)
                     self.set_value(x,y,val+val)
+                    self.score = max(self.score,val+val)
                     self.set_value(x,to_merge,0)
                     y+=1
                 else:
-                    y+=1                  
-         
+                    y+=1                     
     def merge_right(self):
         #from right to left
         for x in range(4):
@@ -205,11 +206,12 @@ class Board():
                 if self.get_value(x,y) == self.get_value(x,to_merge) and self.get_value(x,y)!=0: # need to merge
                     val = self.get_value(x,y)
                     self.set_value(x,y,val+val)
+                    self.score = max(self.score,val+val)
                     self.set_value(x,to_merge,0)
                     y-=2
                 else:
                     y-=1                  
-         
+
     def check_full(self):
         for x in range(4):
             for y in range(4):
@@ -234,8 +236,50 @@ class Board():
             self.move_down()
             self.merge_down()
             self.move_down()
-        self.spawn_new_block()
+        win = self.check_win(64)
+        if win: print('win!')
+        fail = self.check_fail()
+        if not fail:
+            if not self.check_full():
+                self.spawn_new_block()
+        else:print('Fail')
 
+    def check_win(self,target):
+        return self.score >= target
+
+    def check_fail(self):
+        #if not full, not fail yet
+        if not self.check_full():
+            return False
+        #if full but can still make a move, not fail yet
+        else:
+            for row in range(4):
+                for col in range(3):
+                    if self.board[row][col+1] == self.board[row][col]:
+                        return False
+            for col in range(4):
+                for row in range(3):
+                    if self.board[row+1][col] == self.board[row][col]:
+                        return False
+        return True
+
+
+class beauty():
+    #googled
+    blockColor = {
+        0:(204,192,179),
+        2:(238,228,218),
+        4:(237,224,200),
+        8:(242,177,121),
+        16:(245,149,99),
+        32: (246,124,95),
+        64: (246,94,59),
+        128: (237,204,114),
+        256: (237,204,97),
+        512: (237,200,80),
+        1024: (237,197,63),
+        2048: (237,194,46)
+    }
 
 class game_GUI():
     def __init__(self, width, height):
@@ -244,21 +288,42 @@ class game_GUI():
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill((255, 255, 255))
 
+        self.blockColor = {
+        0:(204,192,179),
+        2:(238,228,218),
+        4:(237,224,200),
+        8:(242,177,121),
+        16:(245,149,99),
+        32: (246,124,95),
+        64: (246,94,59),
+        128: (237,204,114),
+        256: (237,204,97),
+        512: (237,200,80),
+        1024: (237,197,63),
+        2048: (237,194,46)
+    }
+
+
     
     def draw_board(self,board):
         #cannot find a elegant way to update the whole screen
+        colorList = beauty.blockColor
         self.screen.fill((255, 255, 255))
-        print(board.board)
-        sysfont = pygame.font.SysFont(None, 30)
+        sysFont = pygame.font.SysFont(None, 30)
         x_skip = self.width//4
         y_skip = self.height//4
         for x in range(4):
             for y in range(4):
+                #draw boardes
+                boarder = pygame.Rect(y*x_skip, x*y_skip, x_skip,  y_skip)
+                pygame.draw.rect(self.screen, color=colorList[0], rect=boarder, width = 100)
+        for x in range(4):
+            for y in range(4):
                 #left and top, so y and x actually
                 block = pygame.Rect(y*x_skip, x*y_skip, x_skip,  y_skip)
-                pygame.draw.rect(self.screen, (30, 222, 236), block, width = 5 )
+                pygame.draw.rect(self.screen,self.blockColor[board.board[x][y]], block, width = 0,border_radius=2 )
                 if board.board[x][y] != 0:
-                    text = sysfont.render("{}".format(board.board[x][y]),True,(0,0,0))
-                    text_coord = text.get_rect(center=(block.centerx, block.centery))
-                    self.screen.blit(text,text_coord)
+                    text = sysFont.render("{}".format(board.board[x][y]),True,(0,0,0))
+                    textCoord = text.get_rect(center=(block.centerx, block.centery))
+                    self.screen.blit(text,textCoord)
         #pygame.display.update()
